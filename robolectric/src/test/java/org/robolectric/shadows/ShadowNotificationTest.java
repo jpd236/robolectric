f8f357edeb718757d688c000a89c8ec9dc6c1026
Jeff Davidson <jpd@google.com>
2017-01-04 11:05:02 -0800

    Add test which demonstrates issue #2801.

--- robolectric/src/test/java/org/robolectric/shadows/ShadowNotificationTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowNotificationTest.java
@@ -6,10 +6,13 @@ import static org.robolectric.RuntimeEnvironment.application;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Shadows;
 import org.robolectric.TestRunners;
+import org.robolectric.annotation.Config;
 
 @RunWith(TestRunners.MultiApiSelfTest.class)
 public class ShadowNotificationTest {
@@ -20,5 +23,14 @@ public class ShadowNotificationTest {
     Notification notification = new Notification();
     notification.setLatestEventInfo(application, "title", "content", pendingIntent);
     assertThat(notification.contentIntent).isSameAs(pendingIntent);
+  }
+
+  @Test
+  @Config(manifest = "src/test/resources/TestAndroidManifestWithTargetSdkN.xml")
+  public void getContentTitle__targetSdkN() throws Exception {
+    assertThat(application.getApplicationInfo().targetSdkVersion).isEqualTo(Build.VERSION_CODES.N);
+    Notification notification =
+        new Notification.Builder(application).setContentTitle("title").build();
+    assertThat(Shadows.shadowOf(notification).getContentTitle().toString()).isEqualTo("title");
   }
 }
